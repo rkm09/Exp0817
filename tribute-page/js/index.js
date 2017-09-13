be1b2fca3c4b69a95c73c0be6261bbe235d6f40c
rkm09 <mishra.raksha09@gmail.com>
2017-09-13 16:29:26 +0530

    Delete index.js

--- tribute-page/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $(".list-group-item").hover(function(){
-    $(this).css("background-color"," 	#cc8500");
-  }); 
-});
-//#e68a00
-//#e6b800
-//hr style:\002665 \00950 \002764
-// #FFD700
